1496747be752120e671e55ddd8ac669adfdb69a3
hua <1828837797@qq.com>
2026-03-15 20:53:12 +0800

    add gitignore

--- Debug/Component/Driver_Layer/driver_crc.d
+++
@@ -1,4 +0,0 @@
-Component/Driver_Layer/driver_crc.o: \
- ../Component/Driver_Layer/driver_crc.c \
- ../Component/Driver_Layer/driver_crc.h
-../Component/Driver_Layer/driver_crc.h:
--- Debug/Component/Driver_Layer/driver_ringbuffer.d
+++
@@ -1,4 +0,0 @@
-Component/Driver_Layer/driver_ringbuffer.o: \
- ../Component/Driver_Layer/driver_ringbuffer.c \
- ../Component/Driver_Layer/driver_ringbuffer.h
-../Component/Driver_Layer/driver_ringbuffer.h:
